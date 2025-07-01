b65bc9a0e42f954e9e22d564de618baccca6f0c7
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-07-01 22:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (97.31%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -24,9 +24,6 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         // If both trees are empty, they are the same.
         if (p == null && q == null) return true;
-
-        // If one of the trees is empty or the values of current nodes don't match,
-        // the trees aren't the same.
         if (p == null || q == null || p.val != q.val) return false;
 
         // Recursively check if the left subtree of both trees are the same 
