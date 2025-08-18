98778f31f966d8addf448480525c33e60e0442df
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-08-18 11:54:28 +0530

    Time: 1 ms (99.78%) | Memory: 42.4 MB (13.60%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.java
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -3,10 +3,10 @@ class Solution {
         int counter =1; 
         int max = 1;
         int i =0;
-        for(int j=1; j<s.length(); j++){
-            // if(i==j){
-            //     continue;
-            // }
+        for(int j=0; j<s.length(); j++){
+             if(i==j){
+                 continue;
+             }
             if(s.charAt(i)==s.charAt(j)){
                 counter++; 
                 max = Math.max(counter, max);
