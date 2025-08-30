25871857a03febfd46f4e3304874539c7a58b241
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-08-30 09:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (96.80%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    /*
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
         if(depth ==1){
             TreeNode new_root = new TreeNode(val);
@@ -43,6 +44,47 @@ class Solution {
             dfs(node.right,val,current+1, depth);
         }
         
+
+    }
+    
+
+    */
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth ==1){
+            TreeNode new_root = new TreeNode(val);
+            new_root.left = root; 
+            return new_root;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int current = 1;
+        while(!queue.isEmpty() && current<depth-1){
+            int level = queue.size();
+            for(int i=0; i<level; i++){
+                TreeNode node = queue.poll();
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+
+            }
+            current++;
+        }
+
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            TreeNode temp_left = node.left;
+            TreeNode temp_right = node.right;
+            node.left = new TreeNode(val);
+            node.left.left = temp_left;
+            node.right = new TreeNode(val);
+            node.right.right = temp_right;
+        }
+        return root;
+
 
     }
 }
