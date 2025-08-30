3aed4eb64d0632b79e0d270292633e3faac27094
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-08-30 08:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (38.40%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth ==1){
+            TreeNode new_root = new TreeNode(val);
+            new_root.left = root;
+            return new_root;
+        }
+
+        dfs(root,val,1,depth);
+        return root;
+    }
+
+    public void dfs(TreeNode node, int val, int current, int depth){
+        if(node==null){
+            return ;
+        }
+        if(current == depth-1){
+            TreeNode temp_left = node.left;
+            TreeNode temp_right = node.right;
+            node.left = new TreeNode(val);
+            node.left.left = temp_left;
+            node.right = new TreeNode(val);
+            node.right.right = temp_right;
+
+        }
+        else{
+            dfs(node.left, val, current+1,depth );
+            dfs(node.right,val,current+1, depth);
+        }
+        
+
+    }
+}
