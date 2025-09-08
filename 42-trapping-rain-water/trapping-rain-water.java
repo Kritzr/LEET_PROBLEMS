d60995131d54aa519f49d2a0675d754a0fc7df74
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-09-08 05:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (8.72%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height ==null || height.length == 0){
+            return 0;
+        }
+
+        int left = 0; 
+        int right =  height.length -1;
+        int max_left = height[left];
+        int max_right = height[right];
+        int result = 0;
+
+        while(left<right){
+            if(max_left<max_right){
+                left++;
+                max_left = Math.max(max_left, height[left]);
+                result+= max_left - height[left];
+
+            }
+
+            else{
+                right --; 
+                max_right = Math.max(max_right, height[right]);
+                result+= max_right - height[right];
+            }
+            
+        }
+        return result;
+    }
+}
