b4d9935954a44a0b77be2b1badee34a210c56d40
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-09-10 23:03:04 +0530

    Time: 2 ms (18.45%) | Memory: 42.1 MB (18.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,20 +1,69 @@
+class TrieNode {
+    public List<TrieNode> children;
+    public int count; 
+    public boolean isleaf;
+
+    public TrieNode() {
+        children = new ArrayList<>(26);
+        for (int i = 0; i < 26; i++) {
+            children.add(null);
+        }
+        count = 0;
+        isleaf = false;
+    }
+}
+
 class Solution {
+    private TrieNode root;
+
+    public Solution() {
+        root = new TrieNode();
+    }
+
+    private void insert(String word) {
+        TrieNode current = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (current.children.get(idx) == null) {
+                current.children.set(idx, new TrieNode());
+                current.count++;
+            }
+            current = current.children.get(idx);
+        }
+        current.isleaf = true;
+    }
+
     public String longestCommonPrefix(String[] strs) {
-        String result = strs[0];
-        if(strs.length==0 ||strs == null){
+        if (strs == null || strs.length == 0) {
             return "";
         }
-        for(int i = 1; i<strs.length;i++){
-            while(!strs[i].startsWith(result)){
-                result = result.substring(0, result.length()-1);
 
-                if(result.isEmpty()){
-                    return "";
-
-                }
+        
+        for (String str : strs) {
+            if (str.isEmpty()) {
+                return "";
             }
         }
 
-        return result;  
+        
+        for (String str : strs) {
+            insert(str);
+        }
+
+        
+        StringBuilder lcp = new StringBuilder();
+        TrieNode current = root;
+
+        
+        while (current != null && current.count == 1 && !current.isleaf) {
+            for (int i = 0; i < 26; i++) {
+                if (current.children.get(i) != null) {
+                    lcp.append((char) ('a' + i));
+                    current = current.children.get(i);
+                    break; 
+                }
+            }
+        }
+        return lcp.toString();
     }
 }
