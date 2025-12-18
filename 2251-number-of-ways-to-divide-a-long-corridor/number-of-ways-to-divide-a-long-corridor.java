095a9e9d12f6a0220992732224ac4f117c223916
Krithika Ravishankar <168807306+Kritzr@users.noreply.github.com>
2025-12-18 06:18:59 +0530

    Time: 306 ms (11.41%) | Memory: 136.2 MB (11.38%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,30 +1,47 @@
 class Solution {
+    private int MOD = 1000000007;
+    private int[][] dp;
     public int numberOfWays(String corridor) {
-        // Store 1000000007 in a variable for convenience
-        final int MOD = 1_000_000_007;
-
-        // Initialize the array to store the result of each sub-problem
-        int[][] count = new int[corridor.length() + 1][3];
-
-        // Base cases
-        count[corridor.length()][0] = 0;
-        count[corridor.length()][1] = 0;
-        count[corridor.length()][2] = 1;
-
-        // Fill the array in a bottom-up fashion
-        for (int index = corridor.length() - 1; index >= 0; index--) {
-            if (corridor.charAt(index) == 'S') {
-                count[index][0] = count[index + 1][1];
-                count[index][1] = count[index + 1][2];
-                count[index][2] = count[index + 1][1];
-            } else {
-                count[index][0] = count[index + 1][0];
-                count[index][1] = count[index + 1][1];
-                count[index][2] = (count[index + 1][0] + count[index + 1][2]) % MOD;
+        int n = corridor.length();
+        dp = new int[n][3];//seats can be 0 or 1 or 2
+        for(int i=0; i<n; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        
+        return dfs(0,0,corridor);
+    }
+        
+    public int dfs(int i, int seats, String corridor){
+        int res =0;
+        if(i==corridor.length()){
+            if(seats ==2){
+                return 1;
+            }
+            else{
+                return 0;
             }
         }
 
-        // Return the result
-        return count[0][0];
+        if(dp[i][seats]!=-1){
+            return dp[i][seats];
+        }
+
+        if(seats==2){
+            if(corridor.charAt(i)=='S'){
+                res = dfs(i+1, 1,corridor);
+            }
+            else{
+                res = (dfs(i+1,0,corridor) + dfs(i+1, 2,corridor))%MOD;
+            }
+        }
+        else {
+            if(corridor.charAt(i)=='S'){
+                res = dfs(i+1, seats+1,corridor);
+            }
+            else{
+                res = dfs(i+1, seats, corridor);
+            }
+        }
+        return dp[i][seats]=res;
     }
 }
